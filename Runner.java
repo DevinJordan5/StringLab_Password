18718194ba7ba3f11c41337512dfa1b7152d9f0f
Jenn Nelson <jnelson@edenpr.org>
2021-02-28 12:30:44 -0600

    initial files

---
+++ Runner.java
@@ -0,0 +1,16 @@
+
+
+public class Runner{
+  
+  public static void main(String[] args){
+    
+    System.out.println("Password passed: " + passwordChecker());
+  
+  }
+  
+  public static boolean passwordChecker(){
+    // this method should keep running until the user says quit, or they successful pick a password
+    
+  }
+  
+}
